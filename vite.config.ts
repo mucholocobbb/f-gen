c16f5ae18e9a728dd26bce3713461a46a1ed3dd3
Egor Matyushkin <e.s.matushkin001@yandex.ru>
2025-12-26 07:52:39 +0400

    f/005 - Поправлены стили и добавлен кастомный слот

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "@/styles/_variables.scss";`,
+        additionalData: `@use "@/styles/_variables.scss" as *;`,
       },
     },
   },
